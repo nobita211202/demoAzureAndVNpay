8f5d3e85772ae564217bbf8fb5e2e23d8ce26982
huy.do <huy.do@bmbsoft.com.vn>
2023-02-24 18:02:45 +0700

    up

---
+++ src/main/java/com/example/demo/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.google.gson.JsonObject;
+import common.Config;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+@RestController
+public class PaymentController {
+
+
+}
--- src/main/java/common/ajaxServlet.java
+++ src/main/java/common/ajaxServlet.java
@@ -1,59 +1,51 @@
-package com.example.demo;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import common.Config;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.*;
 
-@SpringBootApplication
-@RestController
-public class DemoApplication {
+/**
+ *
+ * @author CTT VNPAY
+ */
+public class ajaxServlet extends HttpServlet {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping("/message")
-    public String abc(){
-        return "Đã lên servera";
-    }
-    @PostMapping("/create-payment")
-    public String createPayment(HttpServletRequest req , HttpServletResponse resp)throws IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
         String vnp_Version = "2.1.0";
         String vnp_Command = "pay";
-        String orderType = "Thanh toán hóa đơn";        //đã sửa
-        int amount = 1000000*100;
+        String orderType = req.getParameter("ordertype");
+        long amount = Integer.parseInt(req.getParameter("amount"))*100;
         String bankCode = "NCB";
-
+        
         String vnp_TxnRef = Config.getRandomNumber(8);
         String vnp_IpAddr = Config.getIpAddress(req);
         String vnp_TmnCode = Config.vnp_TmnCode;
-
+        
         Map<String, String> vnp_Params = new HashMap<>();
         vnp_Params.put("vnp_Version", vnp_Version);
         vnp_Params.put("vnp_Command", vnp_Command);
         vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
         vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
-
+        
         if (bankCode != null && !bankCode.isEmpty()) {
             vnp_Params.put("vnp_BankCode", bankCode);
         }
@@ -68,17 +60,17 @@ public class DemoApplication {
             vnp_Params.put("vnp_Locale", "vn");
         }
         vnp_Params.put("vnp_ReturnUrl", Config.vnp_Returnurl);
-        vnp_Params.put("vnp_IpAddr", Config.IPDEFAULT);  //đã sửa
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
+        
         cld.add(Calendar.MINUTE, 15);
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
-
+        
         List fieldNames = new ArrayList(vnp_Params.keySet());
         Collections.sort(fieldNames);
         StringBuilder hashData = new StringBuilder();
@@ -112,7 +104,6 @@ public class DemoApplication {
         job.addProperty("data", paymentUrl);
         Gson gson = new Gson();
         resp.getWriter().write(gson.toJson(job));
-
-        return resp.toString();
     }
+
 }
